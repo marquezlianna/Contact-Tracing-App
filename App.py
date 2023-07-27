513dbca868acab2b1ecd30024fcc41d23eaa14ef
marquezlianna <129668304+marquezlianna@users.noreply.github.com>
2023-07-27 17:40:30 +0800

    create a class

--- App.py
+++ App.py
@@ -4,3 +4,10 @@ import ttkbootstrap as ttk
 from ttkbootstrap.toast import ToastNotification
 from ttkbootstrap.validation import add_regex_validation
 from ttkbootstrap.tableview import Tableview
+
+# Define a class HDF
+class HDF(ttk.Frame):
+    # Constructor method for the class
+    def __init__(self, master_window):
+        # Initialize the parent class constructor
+        super().__init__(master_window, padding=(10, 10, 10, 10))
