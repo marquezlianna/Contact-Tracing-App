4fb1f03a3b5e65b15729ee09cdf4a88b5db38ef4
marquezlianna <129668304+marquezlianna@users.noreply.github.com>
2023-07-27 17:44:01 +0800

    Initialize instance variables

--- App.py
+++ App.py
@@ -15,3 +15,23 @@ class HDF(ttk.Frame):
         # Create a label for the heading
         heading_label = ttk.Label(self, text="Health Declaration Form", font= ('Cascadia Code', 24))
         heading_label.pack(fill=tk.X, pady=10, padx= 25)
+
+# Initialize instance variables
+        self.pack(fill=tk.BOTH, expand=tk.YES)
+        self.name = ttk.StringVar(value="")
+        self.date = ttk.StringVar(value="")
+        self.age = ttk.StringVar(value="")
+        self.gender = ttk.StringVar(value="")
+        self.address = ttk.StringVar(value="")
+        self.phone_number = ttk.StringVar(value="")
+        self.email_address = ttk.StringVar(value="")
+        self.vaccination_status = ttk.StringVar(value="")
+        self.symptoms = ttk.StringVar(value="")
+        self.exposure_status = ttk.StringVar(value="")
+        self.contact_status = ttk.StringVar(value="")
+        self.covid_test_status = ttk.StringVar(value="")
+
+        self.contact_name = ttk.StringVar(value="")
+        self.contact_number = ttk.StringVar(value="")
+        self.contact_email = ttk.StringVar(value="")
+        self.relationship = ttk.StringVar(value="")
