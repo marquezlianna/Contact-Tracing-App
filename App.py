ad7a2358a0fad9a807dd65b41bd85e033603ffce
marquezlianna <129668304+marquezlianna@users.noreply.github.com>
2023-07-27 17:36:41 +0800

    import required libraries

---
+++ App.py
@@ -0,0 +1,6 @@
+# Import required libraries
+import tkinter as tk
+import ttkbootstrap as ttk
+from ttkbootstrap.toast import ToastNotification
+from ttkbootstrap.validation import add_regex_validation
+from ttkbootstrap.tableview import Tableview
